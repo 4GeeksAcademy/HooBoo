326abda4fa9535884d43dba9a46547a270927cd8
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-10-06 15:58:20 +0000

    traer libros para home desde la API LISTO

--- src/front/js/pages/Principal.js
+++ src/front/js/pages/Principal.js
@@ -4,60 +4,67 @@ import Footer from '../component/Footer.jsx';
 import HoobooBanner from '../component/hooboo_banner.jsx';
 import '../../styles/Principal.css';
 
-const genres = {
-    fantasia: [
-        { id: 1, src: 'https://www.unebook.es/blog/wp-content/uploads/2020/05/Juego_de_Tronos-657x1024.jpg', alt: 'Fantasy Book 1' },
-        { id: 2, src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxnAfPHMlcxUBPz31Vev5L_P6Vtm0Y_jU5Bg&s', alt: 'Fantasy Book 2' },
-        { id: 3, src: 'https://www.centrallibrera.com/imagenes/9788419/978841925296.GIF', alt: 'Fantasy Book 3' },
-        { id: 4, src: 'https://www.hola.com/horizon/original_aspect_ratio/e3d17acc42ec-los-dioses-del-norte-la-leyenda-del-bosque-de-jara-santamar-a-montena-.jpg', alt: 'Fantasy Book 4' },
-        { id: 5, src: 'https://libroveolibroleo.com/wp-content/uploads/2020/04/La-viajera-del-tiempo.jpg', alt: 'Fantasy Book 5' },
-        { id: 6, src: 'https://www.centrallibrera.com/imagenes/9788419/978841925296.GIF', alt: 'Fantasy Book 3' },
-        { id: 7, src: 'https://www.centrallibrera.com/imagenes/9788419/978841925296.GIF', alt: 'Fantasy Book 3' }
-    ],
-    romance: [
-        { id: 1, src: 'https://i.pinimg.com/originals/e8/ac/34/e8ac34630ecc6a080f79dfb607f827b8.png', alt: 'Romance Book 1' },
-        { id: 2, src: 'https://img.remediosdigitales.com/89ed5a/a/450_1000.png', alt: 'Romance Book 2' },
-        { id: 3, src: 'https://m.media-amazon.com/images/I/61Z2Zyp2TSL._AC_UF1000,1000_QL80_.jpg', alt: 'Romance Book 3' },
-        { id: 4, src: 'https://img.wattpad.com/cover/5009239-256-k506520.jpg', alt: 'Romance Book 4' },
-        { id: 5, src: 'https://images.cdn1.buscalibre.com/fit-in/360x360/1c/a3/1ca38b639ea1f3f6b49cdcb4bdf5c96d.jpg', alt: 'Romance Book 5' },
-        { id: 6, src: 'https://i.pinimg.com/originals/e8/ac/34/e8ac34630ecc6a080f79dfb607f827b8.png', alt: 'Romance Book 1' },
-        { id: 7, src: 'https://img.remediosdigitales.com/89ed5a/a/450_1000.png', alt: 'Romance Book 2' }
-    ],
-    drama: [
-        { id: 1, src: 'https://www.planetadelibros.com/usuaris/libros/fotos/401/original/portada_con-la-luz-encendida_monica-de-cristobal_202407181015.jpg', alt: 'Drama Book 1' },
-        { id: 2, src: 'https://www.planetadelibros.com/usuaris/libros/thumbs/1f9fb230-25be-4ef2-be0e-446fd74a8405/m_175_310/portada_todo-cambio-ese-verano_elena-pena-bilbao_202402290811.webp', alt: 'Drama Book 2' },
-        { id: 3, src: 'https://st.booknet.com/uploads/covers/220/1677904704_94.jpg', alt: 'Drama Book 3' },
-        { id: 4, src: 'https://www.nosolocine.net/wp-content/uploads/2014/01/La-Ladrona-de-Libros-20131.jpg', alt: 'Drama Book 4' },
-        { id: 5, src: 'https://www.educaciontrespuntocero.com/wp-content/uploads/2022/11/Yo-tu-y-un-quizas-666x1024.jpg', alt: 'Drama Book 5' },
-        { id: 6, src: 'https://st.booknet.com/uploads/covers/220/1677904704_94.jpg', alt: 'Drama Book 3' },
-        { id: 7, src: 'https://www.nosolocine.net/wp-content/uploads/2014/01/La-Ladrona-de-Libros-20131.jpg', alt: 'Drama Book 4' },
-
-    ],
-    thriller: [
-        { id: 1, src: 'https://vanidad.es/images/carpeta_gestor/archivos/2023/07/03/Vanidad_Articulo_libros_thriller_Foto_1.jpeg', alt: 'Thriller Book 1' },
-        { id: 2, src: 'https://static.cegal.es/imagenes/marcadas/9788419/978841962004.gif', alt: 'Thriller Book 2' },
-        { id: 3, src: 'https://www.clarin.com/2021/12/31/t3Ve6O-t0_1200x0__1.jpg', alt: 'Thriller Book 3' },
-        { id: 4, src: 'https://www.estudioenescarlata.com/media/img/portadas/_visd_0001JPG01RNO.jpg', alt: 'Thriller Book 4' },
-        { id: 5, src: 'https://www.emmaglondys.com/wp-content/uploads/2023/07/51bLxVui8hL.jpg', alt: 'Thriller Book 5' },
-        { id: 6, src: 'https://static.cegal.es/imagenes/marcadas/9788419/978841962004.gif', alt: 'Thriller Book 2' },
-        { id: 7, src: 'https://www.clarin.com/2021/12/31/t3Ve6O-t0_1200x0__1.jpg', alt: 'Thriller Book 3' },
-    ],
-};
-
 const Principal = () => {
     const [currentGenre, setCurrentGenre] = useState('fantasia');
     const [isVisible, setIsVisible] = useState(true);
+    const [books, setBooks] = useState({ fantasia: [], romance: [], drama: [], thriller: [] });
 
+
+    const API_KEY = 'AIzaSyDWeHrvToJGuNVbZjPWHcP6C_QDdGNBlbg';
+    const genres = ['fantasia', 'romance', 'drama', 'thriller'];
+    const GENRE_SUBJECTS = {
+        fantasia: 'fantasy',
+        romance: 'romance',
+        drama: 'drama',
+        thriller: 'thriller'
+    };
+
+    const mezclarLibrosPorCategoria = (mezcla) => {
+        for (let i = mezcla.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [mezcla[i], mezcla[j]] = [mezcla[j], mezcla[i]];
+        }
+        return mezcla;
+    };
 
     useEffect(() => {
-        const genreKeys = Object.keys(genres);
+        const getBooksFromApi = async () => {
+            try {
+                const gotBooksFromApi = {};
+                for (const genre of genres) {
+                    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=subject:${GENRE_SUBJECTS[genre]}&maxResults=40&key=${API_KEY}`, {
+                        headers: {
+                            'Authorization': '55948_25703fc2113e4aece39188c265f17591'
+                        }
+                    });
+
+                    const data = await response.json();
+                    gotBooksFromApi[genre] = data.items
+                        .filter(item => item.volumeInfo.imageLinks?.thumbnail)
+                        .map(item => ({
+                            id: item.id,
+                            src: item.volumeInfo.imageLinks.thumbnail,
+                            alt: item.volumeInfo.title
+                        }));
+
+                    gotBooksFromApi[genre] = mezclarLibrosPorCategoria(gotBooksFromApi[genre]);
+                }
+                setBooks(gotBooksFromApi);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+        getBooksFromApi();
+    }, []);
+
+    useEffect(() => {
         const genreInterval = setInterval(() => {
             setIsVisible(false); // Inicia la animación de salida
             setTimeout(() => {
                 setCurrentGenre((prevGenre) => {
-                    const currentIndex = genreKeys.indexOf(prevGenre);
-                    const nextIndex = (currentIndex + 1) % genreKeys.length;
-                    return genreKeys[nextIndex];
+                    const currentIndex = genres.indexOf(prevGenre);
+                    const nextIndex = (currentIndex + 1) % genres.length;
+                    return genres[nextIndex];
                 });
                 setIsVisible(true);
             }, 500);
@@ -70,10 +77,13 @@ const Principal = () => {
         setIsVisible(false);
         setTimeout(() => {
             setCurrentGenre(genre);
+            setBooks(prevBooks => ({
+                ...prevBooks,
+                [genre]: mezclarLibrosPorCategoria([...prevBooks[genre]]) // Mezcla los libros del género actual
+            }));
             setIsVisible(true);
         }, 500);
     };
-
     return (
         <div>
             <HoobooBanner />
@@ -93,7 +103,7 @@ const Principal = () => {
                         </h1>
                     </div>
                     <div className="genre-indicators">
-                        {Object.keys(genres).map((genre) => (
+                        {genres.map((genre) => (
                             <span
                                 key={genre}
                                 className={`indicator ${currentGenre === genre ? 'active' : ''}`}
@@ -102,14 +112,18 @@ const Principal = () => {
                         ))}
                     </div>
                     <div className={`image-grid ${isVisible ? 'fade-in' : 'fade-out'}`}>
-                        {genres[currentGenre].map((book) => (
-                            <img
-                                key={book.id}
-                                src={book.src}
-                                alt={book.alt}
-                                className="book-image"
-                            />
-                        ))}
+                        {books[currentGenre] && books[currentGenre].length > 0 ? (
+                            books[currentGenre].map((book) => (
+                                <img
+                                    key={book.id}
+                                    src={book.src}
+                                    alt={book.alt}
+                                    className="book-image"
+                                />
+                            ))
+                        ) : (
+                            <p>No hay libros disponibles para este género.</p>
+                        )}
                     </div>
                 </div>
             </div>
