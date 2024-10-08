ffd297ada81de9f46397ec69250bd90a4ea82b77
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-10-08 14:49:19 +0000

    banner otras vistas y banner Home LISTO

--- src/front/js/component/BannerOnlyHome.jsx
+++ src/front/js/component/BannerOnlyHome.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import BannerHome3 from "/workspaces/HooBoo/src/front/img/BannerHome3.jpg";
+import LibrosMejor from "/workspaces/HooBoo/src/front/img/LibrosMejor.png";
 import '../../styles/BannerStyleOnlyHome.css';
 import Navbar from "./Navbar.jsx";
 
 const BannerOnlyHome = () => {
     return (
         <div className="banner-container">
-            <img src={BannerHome3} alt="BannerOnlyHome" className="banner-image-only-home" />
+            <img src={LibrosMejor} alt="BannerOnlyHome" className="banner-image-only-home" />
             <div className="bannerHome-text">
                 <h2><strong>HooBoo</strong> </h2>
                 <p className="slogan">Hooked on Bookish</p>
