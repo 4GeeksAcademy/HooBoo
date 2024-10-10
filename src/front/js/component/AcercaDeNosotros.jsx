6386a8080fd297957c7f290455fd4414522f9843
Raúl Santana Castilla <168531613+RaulSantanaCastilla@users.noreply.github.com>
2024-10-10 18:08:45 +0000

    descripcion hecha

--- src/front/js/component/AcercaDeNosotros.jsx
+++ src/front/js/component/AcercaDeNosotros.jsx
@@ -35,7 +35,7 @@ const AcercaDeNosotros = ({ isActive }) => {
       name: 'Raúl',
       role: 'Encargado del Frontend',
       image: raulImage,
-      description: 'Raúl ha hecho el frontend.',
+      description: 'Nacido en Madrid, Raúl siempre ha sido un apasionado de la informática, ya desde pequeño ha estado trasteando con el ordenador ya sea para descubrir nuevos programas y aprender, como modear juegos como por ejemplo Skyrim. Todo esas horas al ordenador le ha llevado a aprender programación y a día de hoy se especializa en frontend aunque con el tiempo y práctica aprenderá sobre backend mucho más también.',
     },
   ];
 
