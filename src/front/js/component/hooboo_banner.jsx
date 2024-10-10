5e364ffa4ae0d93e476e0be5c36d2d182b2e81f8
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-10-10 16:13:05 +0000

    bannerTODOS

--- src/front/js/component/hooboo_banner.jsx
+++ src/front/js/component/hooboo_banner.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import "../../styles/hooboo_banner.css";
+import sincolor from "/workspaces/HooBoo/src/front/img/sincolor.png";
+
+import '../../styles/hooboo_banner.css';
+
 
 const HoobooBanner = () => {
     return (
-        <div className="headerfondo">
-            <div className="header-container">
-
+        <div className="banner-containerHooBoo">
+            <div className="banner-overlay"></div>
+            <img src={sincolor} alt="bannerHoboo" className="banner-image-HooBoo" />
+            <div className="bannerHooboo-text">
+                <p className="textHooBOObanner"><strong>HooBoo</strong> </p>
+                <p className="sloganTextHooboo">Hooked on Bookish</p>
             </div>
         </div>
     );
-};
+}
 
 export default HoobooBanner;
