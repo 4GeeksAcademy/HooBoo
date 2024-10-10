8b37830e970563b740e1722945b998c2e5e5c9dd
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-10-10 11:34:42 +0000

    se agrega boton en acerca de nosotros

--- src/front/js/component/AcercaDeNosotros.jsx
+++ src/front/js/component/AcercaDeNosotros.jsx
@@ -4,6 +4,7 @@ import luisImage from '../../img/Luis.png';
 import marianaImage from '../../img/Mariana.png';
 import patriciaImage from '../../img/Patricia.png';
 import raulImage from '../../img/Raul.png';
+import { Link } from 'react-router-dom';
 
 
 const AcercaDeNosotros = ({ isActive }) => {
@@ -21,10 +22,10 @@ const AcercaDeNosotros = ({ isActive }) => {
       description: 'Mariana ha sido la encargada del backend.',
     },
     {
-      name: 'Luis',
+      name: 'Luis Castilla',
       role: 'Encargado de Backend y Frontend',
       image: luisImage,
-      description: 'Luis se ha encargado del backend y front.',
+      description: 'Luis Castilla es un profesional colombiano de 30 años, actualmente residiendo en España. Con una pasión por la tecnología y el desarrollo de software, ha dedicado su carrera a la creación de soluciones innovadoras tanto en el ámbito del backend como en el frontend. Apasionado por aprender y adaptarse a nuevas tecnologías, su objetivo es seguir creciendo en el mundo de la programación mientras disfruta de la vida en un nuevo país. le encanta trabajar en equipo, compartir conocimientos y siempre busca nuevos retos que impulsen su desarrollo personal y profesional.',
     },
     {
       name: 'Raúl',
@@ -36,16 +37,19 @@ const AcercaDeNosotros = ({ isActive }) => {
 
   return (
     <div className={`teamPage ${isActive ? 'active' : ''}`}>
-      <h1>Conoce a Nuestro Equipo</h1>
+      <h1 className='diseño-deestetitulo'>Conoce a Nuestro Equipo</h1>
       <div className='teamContainer'>
         {teamMembers.map((member, index) => (
           <div key={index} className='memberCard'>
             <img src={member.image} alt={member.name} className='memberImage' />
-            <h3>{member.name}</h3>
-            <p>{member.role}</p>
+            <h3 className='negrita-paralosrole'>{member.name}</h3>
+            <p className='negrita-paralosrole'>{member.role}</p>
             <p>{member.description}</p>
           </div>
         ))}
+      </div>
+      <div className="backButtonContainer">
+        <Link to="/" className="backButton">Volver</Link>
       </div>
     </div>
   );
