23328a5fa2926388e7b292fa8b6057788548520d
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-10-14 10:34:03 +0000

    arreglando ico

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: "../HooBoo/src/front/img/libros.ico",
+        favicon: "/src/front/img/libros.ico",
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
