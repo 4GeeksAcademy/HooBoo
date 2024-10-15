2a1c650a095e00cace5d09ee4001053754d62779
rossmarrlozz <168719962+rossmarrlozz@users.noreply.github.com>
2024-10-15 16:59:27 +0000

    más responsive

--- migrations/versions/fac470eea622_.py
+++ migrations/versions/fac470eea622_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 68bb38dfb6c5
+Revision ID: fac470eea622
 Revises: 
-Create Date: 2024-10-14 10:42:36.746796
+Create Date: 2024-10-15 15:03:31.359052
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '68bb38dfb6c5'
+revision = 'fac470eea622'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/component/Footer.jsx
+++ src/front/js/component/Footer.jsx
@@ -20,84 +20,80 @@ const Footer = () => {
   };
 
   return (
-    <>
-      <footer className="footer">
-        <div className="footer-content">
-          <div className="section social-icons">
-            <a href="https://x.com/HookedonBookish" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="icon twitter-icon">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-            <a href="https://www.instagram.com/hookedonbookish/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="icon instagram-icon">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-            <a href="https://www.facebook.com/profile.php?id=61566675358683" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="icon facebook-icon">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-            <a href="https://www.tiktok.com/@hooked_on_bookish" target="_blank" rel="noopener noreferrer" aria-label="TikTok" className="icon tiktok-icon">
-              <FontAwesomeIcon icon={faTiktok} />
-            </a>
-            <a href="https://open.spotify.com/user/31hbexieokicgypcsfkwcno3im34" target="_blank" rel="noopener noreferrer" aria-label="Spotify" className="icon spotify-icon">
-              <FontAwesomeIcon icon={faSpotify} />
-            </a>
-            <a href="https://es.pinterest.com/HookedonBookish/" target="_blank" rel="noopener noreferrer" aria-label="Pinterest" className="icon pinterest-icon">
-              <FontAwesomeIcon icon={faPinterest} />
-            </a>
-            <a href="https://www.youtube.com/@HookedonBookish" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="icon youtube-icon">
-              <FontAwesomeIcon icon={faYoutube} />
-            </a>
-          </div>
+    <footer className="footer">
+      <div className="section social-icons">
+        <a href="https://x.com/HookedonBookish" target="_blank" rel="noopener noreferrer" aria-label="Twitter" className="icon twitter-icon">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://www.instagram.com/hookedonbookish/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="icon instagram-icon">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://www.facebook.com/profile.php?id=61566675358683" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="icon facebook-icon">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://www.tiktok.com/@hooked_on_bookish" target="_blank" rel="noopener noreferrer" aria-label="TikTok" className="icon tiktok-icon">
+          <FontAwesomeIcon icon={faTiktok} />
+        </a>
+        <a href="https://open.spotify.com/user/31hbexieokicgypcsfkwcno3im34" target="_blank" rel="noopener noreferrer" aria-label="Spotify" className="icon spotify-icon">
+          <FontAwesomeIcon icon={faSpotify} />
+        </a>
+        <a href="https://es.pinterest.com/HookedonBookish/" target="_blank" rel="noopener noreferrer" aria-label="Pinterest" className="icon pinterest-icon">
+          <FontAwesomeIcon icon={faPinterest} />
+        </a>
+        <a href="https://www.youtube.com/@HookedonBookish" target="_blank" rel="noopener noreferrer" aria-label="YouTube" className="icon youtube-icon">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+      </div>
 
-          <div className="section">
-            <Link to="/acerca-de-nosotros" style={{ textDecoration: 'none', color: 'white' }}>
-              <h4 style={{ cursor: 'pointer' }}>Acerca de Nosotros</h4>
-            </Link>
-            <div className="quitar-espacios-footer">
-              <p>~ Conoce a Nuestro Equipo.</p>
-              <p>~ A qué Nos Dedicamos.</p>
-            </div>
-          </div>
-
-          <div className="section">
-            <h4>Contacto</h4>
-            <div className="quitar-espacios-footer">
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px', color: 'white' }} />
-                hooboocontacto@gmail.com
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px', color: 'white' }} />
-                hooboo4geeks@gmail.com
-              </p>
-              <p>
-                <FontAwesomeIcon icon={faWhatsapp} style={{ marginRight: '8px', color: 'green' }} />
-                +34 641 99 2406
-              </p>
-            </div>
-          </div>
-
-          <div className="section">
-            <h4>Servicios</h4>
-            <div className="quitar-espacios-footer">
-              <p>~ Recomendación De Libros.</p>
-              <p>~ Soporte:</p>
-              <p>de Lunes a Viernes de 10:00 AM a 7:00 PM.</p>
-            </div>
-          </div>
-
-          <div className="section rating">
-            <EstrellasValoracion
-              rating={averageRating}
-              onRate={handleRating}
-              isAuthenticated={!!store.token}
-            />
-            <div className="rating-text">
-              <p>Valoración Media: {averageRating} / 5</p>
-              <p>{totalVotes} Usuarios han votado</p>
-            </div>
-          </div>
+      <div className="section">
+        <Link to="/acerca-de-nosotros" className="link-acerca" style={{ textDecoration: 'none' }}>
+          <h4 style={{ cursor: 'pointer' }}>Acerca de Nosotros</h4>
+        </Link>
+        <div className="quitar-espacios-footer">
+          <p>~ Conoce a Nuestro Equipo.</p>
+          <p>~ A qué Nos Dedicamos.</p>
         </div>
-      </footer>
-    </>
+      </div>
+
+      <div className="section">
+        <h4>Contacto</h4>
+        <div className="quitar-espacios-footer">
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px', color: 'white' }} />
+            hooboocontacto@gmail.com
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: '8px', color: 'white' }} />
+            hooboo4geeks@gmail.com
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faWhatsapp} style={{ marginRight: '8px', color: 'green' }} />
+            +34 641 99 2406
+          </p>
+        </div>
+      </div>
+
+      <div className="section">
+        <h4>Servicios</h4>
+        <div className="quitar-espacios-footer">
+          <p>~ Recomendación De Libros.</p>
+          <p>~ Soporte:</p>
+          <p>de Lunes a Viernes de 10:00 AM a 7:00 PM.</p>
+        </div>
+      </div>
+
+      <div className="section rating">
+        <EstrellasValoracion
+          rating={averageRating}
+          onRate={handleRating}
+          isAuthenticated={!!store.token}
+        />
+        <div className="rating-text">
+          <p>Valoración Media: {averageRating} / 5</p>
+          <p>{totalVotes} Usuarios han votado</p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
